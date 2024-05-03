ff962e240adadd86c5cdfbe9f567e063a25abf22
saiganesh71362 <saiganesh71362@gmail.com>
2024-05-03 18:53:26 +0530

    Add Swagger Configuration

---
+++ src/main/java/com/product/swagger/ProductSwaggerConfiguration.java
@@ -0,0 +1,20 @@
+package com.product.swagger;
+
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ProductSwaggerConfiguration {
+	
+	@Bean
+	public GroupedOpenApi controllerApi()
+	{
+	        return GroupedOpenApi.builder()
+	                .group("Product-Management")
+	                .packagesToScan("com.product.controller")
+	                .build();
+	 }
+
+
+}
